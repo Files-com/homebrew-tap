ee208e9391e8bb5234f966c4551c5132a3a1d419
Dustin Zeisler <dustin@zeisler.net>
2022-12-16 09:29:11 -0800

    Display full changelog since last version

--- Formula/files-cli-head.rb
+++ Formula/files-cli-head.rb
@@ -6,6 +6,7 @@ class FilesCliHead < Formula
   depends_on "go" => :build
   
   def install
+    @last_version = `#{bin}/files-cli -v --ignore-version-check`.chomp rescue ""
     ENV["GOPATH"] = buildpath
     ENV["CGO_ENABLED"] = "1"
     bin_path = buildpath/"src/github.com/Files-com/files-cli"
@@ -16,6 +17,11 @@ class FilesCliHead < Formula
       system "go", "mod", "download"
       system "go", "build", "-o", bin/"files-cli", "."
     end
+  end
+  
+  def post_install
+    current_version = `#{bin}/files-cli --v --ignore-version-check`
+    puts `#{bin}/files-cli changelog "#{[@last_version, current_version].reject(&:nil?).join("..")}" --ignore-version-check`
   end
   
   test do
