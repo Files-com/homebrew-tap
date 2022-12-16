87f62f7010b23acadc641fef76f1f7cd57586202
Dustin Zeisler <dustin@zeisler.net>
2022-12-15 17:00:37 -0800

    Return output of changelog

--- Formula/files-cli.rb
+++ Formula/files-cli.rb
@@ -70,7 +70,7 @@ class FilesCli < Formula
   end
 
   def post_install
-    system %Q[files-cli changelog "$(files-cli -v)" --ignore-version-check]
+    puts `files-cli changelog "$(files-cli -v)" --ignore-version-check`
   end
 
   test do
